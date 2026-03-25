f430a3f1c35da52bb73d7c36c033f71c6f26ef07
Bochewee <florian.huguet78@orange.fr>
2026-03-25 12:53:12 +0100

    Ajout de la méthode rank_matter_1 et appel dans le main

--- students.py
+++ students.py
@@ -14,8 +14,14 @@ class SchoolClass:
     def add_student(self, student: Student):
         self.etudiants.append(student)
 
+    def rank_matter_1(self):
+        return sorted(self.etudiants, key=lambda s: s.notes[0], reverse=True)
+
 
 school_class = SchoolClass()
 school_class.add_student(Student('J', 10, 12, 13))
 school_class.add_student(Student('A', 8, 2, 17))
 school_class.add_student(Student('V', 9, 14, 14))
+
+for s in school_class.rank_matter_1():
+    print(f"{s.nom} : {s.notes[0]}")
